b4069bccf91d48d9cab928baa4ce82b6c6f3cde6
abeneyto <alvarobeneytohurtado@gmail.com>
2019-10-22 20:03:54 +0200

    Ejercicio3 funcionando

--- app/src/main/java/com/example/hello/ProfileActivity.kt
+++ app/src/main/java/com/example/hello/ProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -37,4 +38,5 @@ class ProfileActivity : AppCompatActivity() {
 
 
     }
+
 }
